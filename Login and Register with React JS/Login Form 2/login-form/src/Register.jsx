b53f1721e63a20804315eb0743d872cd50e71bdf
Aakash <117282517+AakashTheDev@users.noreply.github.com>
2023-10-14 11:31:45 +0530

    Update Register.jsx

--- Login and Register with React JS/Login Form 2/login-form/src/Register.jsx
+++ Login and Register with React JS/Login Form 2/login-form/src/Register.jsx
@@ -26,48 +26,8 @@ function Register() {
         <input required type='text' placeholder='Phonenumber'/>
         
         <button>Verify Otp</button>
-
-        <label>Relationship</label>
-
-        <select>
-        <option value="select">Select</option>
-            <option value="student">Student</option>
-            <option value="father">Father</option>
-            <option value="mother">Mother</option>
-            <option value="teacher">Teacher</option>
-            <option value="others">Others</option>
-        </select>
-
-        <label>OTP Verification Code</label>
-
-        <input required type='text'/>
-
-        <label>What is Your Group in +2</label>
-
-        <input required type='text'/>
-        
-        <label>What is Your Mark in +2</label>
-
-        <input required type='number'/>
-        
-        <label>Board of Education</label>
-
-        <select>
-            <option value="select">Select</option>
-            <option value="stateboard">Stateboard</option>
-            <option value="cbse">CBSE</option>
-            <option value="others">Others</option>
-        </select>
-              
-        <label>School Type</label>
-
-        <select>
-            <option value="government">Government</option>
-            <option value="governmentaided">Government Aided</option>
-            <option value="private">Private</option>
-        </select>
-
-        <label>School Name</label>
+
+        <label>Unique User Name</label>
 
         <input required type='text'/>
 
